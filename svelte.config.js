b98b333131cb4eb3b2d1ed547b3631e896792bb4
Hüseyin Akçiçek <huseyin.akcicek@outlook.com>
2025-09-25 08:14:43 +0300

    test

--- svelte.config.js
+++ svelte.config.js
@@ -3,23 +3,10 @@ import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
+	// Consult https://svelte.dev/docs/kit/integrations
+	// for more information about preprocessors
 	preprocess: vitePreprocess(),
-	kit: {
-		adapter: adapter({
-			// See below for an explanation of these options
-			config: undefined,
-			platformProxy: {
-				configPath: undefined,
-				environment: undefined,
-				persist: undefined
-			},
-			fallback: 'plaintext',
-			routes: {
-				include: ['/*'],
-				exclude: ['<all>']
-			}
-		})
-	}
+	kit: { adapter: adapter() }
 };
 
 export default config;
